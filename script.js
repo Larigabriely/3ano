e976bece74c46e2d4d8767877c8a96bc3d236f3b
Larigabriely <143524767+Larigabriely@users.noreply.github.com>
2025-04-08 16:23:26 -0300

    02

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte'); 
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte'); 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    let tamanhoAtualFonte = 1;
+    // Verificando se os botões existem no DOM
+    if (aumentaFonteBotao && diminuiFonteBotao) {
+        let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function() {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`; 
-    });
+        aumentaFonteBotao.addEventListener('click', function() {
+            tamanhoAtualFonte += 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        });
 
-    diminuiFonteBotao.addEventListener('click', function() {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`; 
-    });
+        diminuiFonteBotao.addEventListener('click', function() {
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        });
+    } else {
+        console.error('Botões não encontrados no DOM');
+    }
 });
