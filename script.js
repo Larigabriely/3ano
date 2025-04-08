43f6a3fb210d76fbc7dab7d3ffb934536d8570e8
Larigabriely <143524767+Larigabriely@users.noreply.github.com>
2025-04-08 16:17:26 -0300

    22

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte'); // Corrigido aqui
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte'); 
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte'); 
 
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function() {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`; // Adicionada a chave que estava faltando
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`; 
     });
 });
