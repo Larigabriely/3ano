fcc346665e96862dd9edce7cd02abb9d12944dbc
Larigabriely <143524767+Larigabriely@users.noreply.github.com>
2025-04-09 14:27:00 -0300

    55

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const botaoDeAcessibilidade = document.getElementById ('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener ('click, function'() {
+        botaoDeAcessibilidade.classlist.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classlist.toggle('apresenta-lista')
+    })
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
